101c7ae6686c0203357b4157a3578a7788abb65a
marcelo.anseldev@hotmail.com <marcelo.anseldev@hotmail.com>
2023-07-07 14:33:37 -0300

    Deploy _1.1

---
+++ index.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal(".header-title",{ duration: 1000 })
+sr.reveal(".header-text",{ duration: 3000})
+
+sr.reveal(".text-info",{
+  rotate: { x: 0, y: 80, z: 0},
+  duration: 5000
+})
+
+sr.reveal("input", {
+  rotate: {x: 80, y: 0, z: 0},
+  duration: 1000
+})
+
+sr.reveal("#title-form", { duration: 1000 }) 
+
